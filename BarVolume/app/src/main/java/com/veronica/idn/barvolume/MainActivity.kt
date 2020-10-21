9945e84329082ae4d217de61e71d2018b8a75eaf
veroanggra <veronicaputri497@gmail.com>
2020-10-21 19:02:34 +0700

    Initial commit

---
+++ BarVolume/app/src/main/java/com/veronica/idn/barvolume/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.veronica.idn.barvolume
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
