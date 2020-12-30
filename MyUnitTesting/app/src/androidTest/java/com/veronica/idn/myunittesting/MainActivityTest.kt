f4146b22d35e2aafda9c737782147ddfc4a49b99
veroanggra <veronicaputri497@gmail.com>
2020-12-30 18:46:29 +0700

    testing-cub all

--- MyUnitTesting/app/src/androidTest/java/com/veronica/idn/myunittesting/MainActivityTest.kt
+++ MyUnitTesting/app/src/androidTest/java/com/veronica/idn/myunittesting/MainActivityTest.kt
@@ -43,18 +43,64 @@ class MainActivityTest {
     }
 
     @Test
-    fun getCuboidViewModel() {
+    fun assertGetSurfaceArea() {
+        onView(withId(R.id.et_length)).perform(typeText(dummyLength), closeSoftKeyboard())
+        onView(withId(R.id.et_width)).perform(typeText(dummyWidth), closeSoftKeyboard())
+        onView(withId(R.id.et_heigth)).perform(typeText(dummyHeight), closeSoftKeyboard())
+
+        onView(withId(R.id.btn_surface_area)).check(matches(isDisplayed()))
+        onView(withId(R.id.btn_surface_area)).perform(click())
+
+        onView(withId(R.id.btn_surface_area)).check(matches(isDisplayed()))
+        onView(withId(R.id.btn_surface_area)).perform(click())
+
+        onView(withId(R.id.tv_result)).check(matches(isDisplayed()))
+        onView(withId(R.id.tv_result)).check(matches(withText(dummySurfaceArea)))
+
     }
 
     @Test
-    fun setCuboidViewModel() {
+    fun assertGetVolume(){
+        onView(withId(R.id.et_length)).perform(typeText(dummyLength), closeSoftKeyboard())
+        onView(withId(R.id.et_width)).perform(typeText(dummyWidth), closeSoftKeyboard())
+        onView(withId(R.id.et_heigth)).perform(typeText(dummyHeight), closeSoftKeyboard())
+
+        onView(withId(R.id.btn_save)).check(matches(isDisplayed()))
+        onView(withId(R.id.btn_save)).perform(click())
+
+        onView(withId(R.id.btn_volume)).check(matches(isDisplayed()))
+        onView(withId(R.id.btn_volume)).check(matches(withText(dummyVolume)))
+
     }
 
     @Test
-    fun onCreate() {
-    }
+    fun assertEmptyInput (){
+        //check length
+        onView(withId(R.id.et_length)).perform(typeText(emptyInput), closeSoftKeyboard())
+        onView(withId(R.id.btn_save)).check(matches(isDisplayed()))
+        onView(withId(R.id.btn_save)).perform(click())
 
-    @Test
-    fun onClick() {
+        onView(withId(R.id.et_length)).check(matches(hasErrorText(fieldEmpty)))
+        onView(withId(R.id.et_length)).perform(typeText(dummyLength), closeSoftKeyboard())
+
+        //check width
+        onView(withId(R.id.et_width)).perform(typeText(emptyInput), closeSoftKeyboard())
+        onView(withId(R.id.btn_save)).check(matches(isDisplayed()))
+        onView(withId(R.id.btn_save)).perform(click())
+
+        onView(withId(R.id.et_width)).check(matches(hasErrorText(fieldEmpty)))
+        onView(withId(R.id.et_width)).perform(typeText(dummyWidth), closeSoftKeyboard())
+
+        //check height
+        onView(withId(R.id.et_heigth)).perform(typeText(emptyInput), closeSoftKeyboard())
+        onView(withId(R.id.btn_save)).check(matches(isDisplayed()))
+        onView(withId(R.id.btn_save)).perform(click())
+
+        onView(withId(R.id.et_heigth)).check(matches(hasErrorText(fieldEmpty)))
+        onView(withId(R.id.et_heigth)).perform(typeText(dummyHeight), closeSoftKeyboard())
+
     }
+    
+
+
 }
