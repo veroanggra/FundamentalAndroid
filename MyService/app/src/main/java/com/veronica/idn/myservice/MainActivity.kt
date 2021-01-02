a44eae2db03b114b1056d204c366d4ef8b35a273
veroanggra <veronicaputri497@gmail.com>
2021-01-02 22:45:15 +0700

    service

---
+++ MyService/app/src/main/java/com/veronica/idn/myservice/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.veronica.idn.myservice
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnStartService = findViewById<Button>(R.id.btn_start_service)
+        btnStartService.setOnClickListener {
+            val mStartServiceIntent = Intent(this, MyService::class.java)
+            startService(mStartServiceIntent)
+        }
+
+        val btnStartJobIntentService = findViewById<Button>(R.id.btn_start_intent_service)
+        btnStartJobIntentService.setOnClickListener {
+
+        }
+        val btnStartBoundService = findViewById<Button>(R.id.btn_start_bound_service)
+        btnStartBoundService.setOnClickListener {
+
+        }
+        val btnStopBoundService = findViewById<Button>(R.id.btn_stop_bound_service)
+        btnStopBoundService.setOnClickListener { }
+    }
+}
