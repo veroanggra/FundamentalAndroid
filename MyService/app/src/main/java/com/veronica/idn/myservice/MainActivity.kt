1263d05018d0306f17d44a22d40c82711bd20e3b
veroanggra <veronicaputri497@gmail.com>
2021-01-04 12:50:06 +0700

    service bound

--- MyService/app/src/main/java/com/veronica/idn/myservice/MainActivity.kt
+++ MyService/app/src/main/java/com/veronica/idn/myservice/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.veronica.idn.myservice
 
+import android.content.ComponentName
 import android.content.Intent
+import android.content.ServiceConnection
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.IBinder
 import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    private var mServiceBound = false
+    private lateinit var mBoundService: MyBoundService
+
+    private val mServiceConnection = object : ServiceConnection {
+        override fun onServiceConnected(p0: ComponentName?, p1: IBinder) {
+            val myBinder = p1 as MyBoundService.MyBinder
+            mBoundService = myBinder.getService
+            mServiceBound = true
+        }
+
+        override fun onServiceDisconnected(p0: ComponentName) {
+            mServiceBound = false
+        }
+
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -25,9 +44,20 @@ class MainActivity : AppCompatActivity() {
         }
         val btnStartBoundService = findViewById<Button>(R.id.btn_start_bound_service)
         btnStartBoundService.setOnClickListener {
+            val mBoundServiceIntent = Intent(this, MyBoundService::class.java)
+            bindService(mBoundServiceIntent, mServiceConnection, BIND_AUTO_CREATE)
 
         }
         val btnStopBoundService = findViewById<Button>(R.id.btn_stop_bound_service)
-        btnStopBoundService.setOnClickListener { }
+        btnStopBoundService.setOnClickListener {
+            unbindService(mServiceConnection)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if (mServiceBound){
+            unbindService(mServiceConnection)
+        }
     }
 }
