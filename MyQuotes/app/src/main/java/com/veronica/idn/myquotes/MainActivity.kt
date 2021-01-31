d143dbfb4f7acd1e6c838cf77cf338c2ceb2eede
veroanggra <veronicaputri497@gmail.com>
2021-01-31 23:30:41 +0700

    make view

--- MyQuotes/app/src/main/java/com/veronica/idn/myquotes/MainActivity.kt
+++ MyQuotes/app/src/main/java/com/veronica/idn/myquotes/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.veronica.idn.myquotes
 
+import android.os.Binder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.veronica.idn.myquotes.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        private val TAG = MainActivity::class.java.simpleName
+    }
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        getRandomQuotes()
+    }
+
+    private fun getRandomQuotes() {
     }
 }
