3030b9290bbea88e5bf383386ac0608c9f105078
veroanggra <veronicaputri497@gmail.com>
2021-02-16 22:53:11 +0700

    add detail

--- MyDeepNavigation/app/src/main/java/com/veronica/idn/mydeepnavigation/MainActivity.kt
+++ MyDeepNavigation/app/src/main/java/com/veronica/idn/mydeepnavigation/MainActivity.kt
@@ -1,11 +1,47 @@
 package com.veronica.idn.mydeepnavigation
 
+import android.app.PendingIntent
+import android.app.TaskStackBuilder
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.veronica.idn.mydeepnavigation.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnOpenDetail.setOnClickListener(this)
+
+        showNotification(this, getString(R.string.notification_title), getString(R.string.notification_message), 110)
+    }
+
+    private fun showNotification(context: Context, title: String, message: String, notifId: Int) {
+        val CHANNEL_ID = "Channel_1"
+        val CHANNEL_NAME = "Navigation channel"
+
+        val notifDetailIntent = Intent(this, DetailActivity::class.java)
+        notifDetailIntent.putExtra(DetailActivity.EXTRA_TITLE, title)
+        notifDetailIntent.putExtra(DetailActivity.EXTRA_MESSAGE, message)
+
+        val pendingIntent = TaskStackBuilder.create(this)
+                .addParentStack(DetailActivity::class.java)
+                .addNextIntent(notifDetailIntent)
+                .getPendingIntent(110, PendingIntent.FLAG_UPDATE_CURRENT)
+
+    }
+
+    override fun onClick(v: View) {
+        if (v.id == R.id.btn_open_detail) {
+            val detailIntent = Intent(this, DetailActivity::class.java)
+            detailIntent.putExtra(DetailActivity.EXTRA_TITLE, getString(R.string.detail_title))
+            detailIntent.putExtra(DetailActivity.EXTRA_MESSAGE, getString(R.string.detail_message))
+            startActivity(detailIntent)
+        }
     }
 }
