f215028bd8ca261190152f09bab5526a87defddb
veroanggra <veronicaputri497@gmail.com>
2021-03-08 22:52:24 +0700

    mainviewmodel

--- ViewModelSecond/app/src/main/java/com/veronica/idn/viewmodelsecond/MainViewModel.kt
+++ ViewModelSecond/app/src/main/java/com/veronica/idn/viewmodelsecond/MainViewModel.kt
@@ -1,8 +1,17 @@
 package com.veronica.idn.viewmodelsecond
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
     val listWeather = MutableLiveData<ArrayList<WeatherItems>>()
+    fun setWeather (cities : String){
+        val listItems = ArrayList<WeatherItems>()
+
+
+    }
+    fun getWeather() : LiveData<ArrayList<WeatherItems>>{
+        return  listWeather
+    }
 }
